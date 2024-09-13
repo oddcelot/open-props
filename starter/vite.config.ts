4253985c764747c114f5a49d70573aac41f4a724
Stefan Kopco <stefan.kopco@vent.io>
2024-09-13 21:55:40 +0200

    add lightning css

---
+++ starter/vite.config.ts
@@ -0,0 +1,31 @@
+import browserslist from 'browserslist'
+import { defineConfig } from 'vite'
+import { browserslistToTargets, bundleAsync } from 'lightningcss'
+
+export const config = defineConfig({
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			targets: browserslistToTargets(browserslist('>= 0.25%')),
+			drafts: { customMedia: true },
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
+	plugins: [
+		{
+			name: 'css-additional-data',
+			enforce: 'pre',
+			transform(code, id) {
+				if (id.endsWith('.css')) {
+					// probably shouldn't inject `@custom-media` directly since it can cause an error such as:
+					//   @import rules must precede all rules aside from @charset and @layer statements
+					return `@import "/src/custom-media.css";` + code
+				}
+			},
+		},
+	],
+})
+
+export default config
