657ef622591421a4399fdf72ce3bf6416b5d3cb4
Stefan Kopco <hey@oddcelot.dev>
2024-12-31 11:41:22 +0100

    WIP

---
+++ starter-react/vite.config.ts
@@ -0,0 +1,36 @@
+import browserslist from 'browserslist'
+import { defineConfig } from 'vite'
+import { browserslistToTargets, bundleAsync } from 'lightningcss'
+import { Features } from 'lightningcss'
+
+export const config = defineConfig({
+	css: {
+		transformer: 'lightningcss',
+		lightningcss: {
+			// targets: browserslistToTargets(browserslist('>= 0.25%')),
+			drafts: { customMedia: true },
+			exclude: Features.OklabColors,
+		},
+	},
+	build: {
+		cssMinify: 'lightningcss',
+	},
+	plugins: [
+		{
+			name: 'css-additional-data',
+			enforce: 'pre',
+			transform(code, id) {
+				if (id.endsWith('.css')) {
+					// probably shouldn't inject `@custom-media` directly since it can cause an error such as:
+					//   @import rules must precede all rules aside from @charset and @layer statements
+					return (
+						`@import "../css/media-queries.css";@import "../css/browser-queries.css";` +
+						code
+					)
+				}
+			},
+		},
+	],
+})
+
+export default config
